80be7ee6a7cc01a0d21aeda9d32c491495983630
Deb-das01 <97828828+Deb-das01@users.noreply.github.com>
2024-07-28 00:09:47 +0530

    Update apiCalls.js

--- src/context/authContext/apiCalls.js
+++ src/context/authContext/apiCalls.js
@@ -5,7 +5,7 @@ export const login = async (user, dispatch) => {
   dispatch(loginStart());
   try {
     //Below line makes a request to login the user along with the user credentials passed with the BODY.
-    const res = await axios.post("auth/login", user);
+    const res = await axios.post("https://netflix-mern-backend.onrender.com/api/auth/login", user);
     res.data.isAdmin && dispatch(loginSuccess(res.data));
   } catch (err) {
     dispatch(loginFailure());
